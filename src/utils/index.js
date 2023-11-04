39353497456ec5fd094479b6c659660b4d89e2fe
nainoktv <nainoaktvillegas@gmail.com>
2023-11-03 22:09:22 -0700

    create nav options for customer and admin

---
+++ src/utils/index.js
@@ -0,0 +1,40 @@
+export const navOptions = [
+  {
+    id: "home",
+    label: "Home",
+    path: "/",
+  },
+  {
+    id: "listing",
+    label: "All Products",
+    path: "/products/listing/all-products",
+  },
+  {
+    id: "listingMen",
+    label: "Men",
+    path: "/products/listing/men",
+  },
+  {
+    id: "listingWomen",
+    label: "Women",
+    path: "/products/listing/women",
+  },
+  {
+    id: "listingKids",
+    label: "Kids",
+    path: "/products/listing/kids",
+  },
+];
+
+export const adminNavOptions = [
+  {
+    id: "adminListing",
+    label: "Manage All Products",
+    path: "/admin-view/all-products",
+  },
+  {
+    id: "adminNewProduct",
+    label: "Add New Product",
+    path: "/admin-view/add-products",
+  },
+];
