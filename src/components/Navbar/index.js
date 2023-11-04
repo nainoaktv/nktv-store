73ef34452934d2b36c1199d72b0bc161bc5a0a2d
nainoktv <nainoaktvillegas@gmail.com>
2023-11-03 22:35:35 -0700

    add nav options to navbar and set mobile functionality

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,4 +1,98 @@
 "use client";
+
+import { adminNavOptions, navOptions } from "@/utils";
+import { Fragment } from "react";
+
+const isAdminView = false;
+const isAuthUser = false;
+const user = {
+  role: "admin",
+};
+
+function NavItems() {
+  return (
+    <div
+      className="items-center justify-between w-full md:flex md:w-auto"
+      id="nav-items"
+    >
+      <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium font-sans md:flex-row md:space-x-8 md:mt-0 md:border-0">
+        {isAdminView
+          ? adminNavOptions.map((item) => (
+              <li
+                className="cursor-pointer block py-2 pl-3 pr-4 text-black md:p-0"
+                key={item.id}
+              >
+                {item.label}
+              </li>
+            ))
+          : navOptions.map((item) => (
+              <li
+                className="cursor-pointer block py-2 pl-3 pr-4 text-black md:p-0"
+                key={item.id}
+              >
+                {item.label}
+              </li>
+            ))}
+      </ul>
+    </div>
+  );
+}
+
 export default function Navbar() {
-  return <div>Navbar</div>;
+  return (
+    <>
+      <nav className="bg-white fixed w-full z-20 border-b border-gray-400">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <div className="flex items-center cursor-pointer">
+            <span className="self-center text-2xl font-semibold whitespace-nowrap text-black">
+              NKTV Store
+            </span>
+          </div>
+          <div className="flex md:order-2 gap-2">
+            {!isAdminView && isAuthUser ? (
+              <Fragment>
+                <button className="btn-text">Account</button>
+                <button className="btn-text">Cart</button>
+              </Fragment>
+            ) : null}
+            {user?.role === "admin" ? (
+              isAdminView ? (
+                <button className="btn-text">Customer View</button>
+              ) : (
+                <button className="btn-text">Admin View</button>
+              )
+            ) : null}
+            {isAuthUser ? (
+              <button className="btn-text">Logout</button>
+            ) : (
+              <button className="btn-text">Login</button>
+            )}
+            <button
+              data-collapse-toggle="navbar-sticky"
+              type="button"
+              className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+              aria-controls="navbar-sticky"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+            </button>
+          </div>
+          <NavItems />
+        </div>
+      </nav>
+    </>
+  );
 }
