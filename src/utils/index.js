b46c07171ac3283dc95e910330a5d770acbece8d
nainoktv <nainoaktvillegas@gmail.com>
2023-11-10 21:15:36 -0800

    create registration form controls array

--- src/utils/index.js
+++ src/utils/index.js
@@ -38,3 +38,51 @@ export const adminNavOptions = [
     path: "/admin-view/add-products",
   },
 ];
+
+export const registrationFormControls = [
+  {
+    id: "name",
+    type: "text",
+    placeholder: "Enter your name",
+    label: "Name",
+    componentType: "input",
+  },
+  {
+    id: "email",
+    type: "email",
+    placeholder: "Enter your email",
+    label: "Email",
+    componentType: "input",
+  },
+  {
+    id: "name",
+    type: "text",
+    placeholder: "Enter your name",
+    label: "Name",
+    componentType: "input",
+  },
+  {
+    id: "password",
+    type: "password",
+    placeholder: "Enter your password",
+    label: "Password",
+    componentType: "input",
+  },
+  {
+    id: "role",
+    type: "",
+    placeholder: "",
+    label: "Role",
+    componentType: "select",
+    option: [
+      {
+        id: "admin",
+        label: "Admin",
+      },
+      {
+        id: "customer",
+        label: "Customer",
+      },
+    ],
+  },
+];
