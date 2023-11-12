1a1542f5b51c0d6eea4e95ce61307f0f81331ca2
nainoktv <nainoaktvillegas@gmail.com>
2023-11-12 14:57:47 -0800

    remove deprecations

--- src/database/index.js
+++ src/database/index.js
@@ -1,15 +1,10 @@
 import mongoose from "mongoose";
 
-const configOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-
 const dbConnection = async () => {
   const connectionUrl = process.env.DATABASE_URL;
 
   mongoose
-    .connect(connectionUrl, configOptions)
+    .connect(connectionUrl)
     .then(() => console.log("Connected to database!"))
     .catch((err) =>
       console.log(`Getting error from db connection: ${err.message}`)
