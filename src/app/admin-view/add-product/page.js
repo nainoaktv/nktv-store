b8cd68617876b9b4eb2580ea8af1c028515b6e02
nainoktv <nainoaktvillegas@gmail.com>
2023-11-14 01:43:07 -0800

    add product form control components

--- src/app/admin-view/add-product/page.js
+++ src/app/admin-view/add-product/page.js
@@ -1,7 +1,44 @@
+"use client";
+
+import InputComponent from "@/components/FormElements/InputComponent";
+import SelectComponent from "@/components/FormElements/SelectComponent";
+import TileComponent from "@/components/TileComponent";
+import { adminAddProductFormControls, availableSizes } from "@/utils";
+
 export default function AdminAddNewProduct() {
+  // TODO: Left off here
+  function handleImage() {}
+
   return (
-    <div>
-      <h1>Add New Product</h1>
+    <div className="w-full mt-5 mr-0 mb-0 ml-0 relative">
+      <div className="flex flex-col items-start justify-start p-10 bg-white shadow-2xl rounded-xl relative">
+        <div className="w-full mt-6 mr-0 mb-0 ml-0 space-y-8 text-black">
+          <input
+            accept="image/*"
+            max="1000000"
+            type="file"
+            onChange={handleImage}
+          />
+          <div className="flex gap-2 flex-col">
+            <label>Available Sizes</label>
+            <TileComponent data={availableSizes} />
+          </div>
+          {adminAddProductFormControls.map((controlItem) =>
+            controlItem.componentType === "input" ? (
+              <InputComponent
+                type={controlItem.type}
+                placeholder={controlItem.placeholder}
+                label={controlItem.label}
+              />
+            ) : controlItem.componentType === "select" ? (
+              <SelectComponent
+                label={controlItem.label}
+                options={controlItem.options}
+              />
+            ) : null
+          )}
+        </div>
+      </div>
     </div>
   );
 }
