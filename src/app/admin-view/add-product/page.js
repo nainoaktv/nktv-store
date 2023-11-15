84721818ac1c22b559dae8f4d4856a830a551b9f
nainoktv <nainoaktvillegas@gmail.com>
2023-11-14 19:13:22 -0800

    successfully upload images to firebase and get url response

--- src/app/admin-view/add-product/page.js
+++ src/app/admin-view/add-product/page.js
@@ -3,11 +3,60 @@
 import InputComponent from "@/components/FormElements/InputComponent";
 import SelectComponent from "@/components/FormElements/SelectComponent";
 import TileComponent from "@/components/TileComponent";
-import { adminAddProductFormControls, availableSizes } from "@/utils";
+import {
+  adminAddProductFormControls,
+  availableSizes,
+  firebaseConfig,
+  firebaseStorageUrl,
+} from "@/utils";
+import { initializeApp } from "firebase/app";
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from "firebase/storage";
+
+const app = initializeApp(firebaseConfig);
+const storage = getStorage(app, firebaseStorageUrl);
+
+const createUniqueFileName = (getFile) => {
+  const timeStamp = Date.now();
+  const randomStringValue = Math.random().toString(36).substring(2, 12);
+
+  return `${getFile.name}-${timeStamp}-${randomStringValue}`;
+};
+
+async function helperForUploadingImageToFirebase(file) {
+  const getFileName = createUniqueFileName(file);
+  const storageReference = ref(storage, `nktv-store/${getFileName}`);
+  const uploadImage = uploadBytesResumable(storageReference, file);
+
+  return new Promise((resolve, reject) => {
+    uploadImage.on(
+      "state_changed",
+      (snapshot) => {},
+      (error) => {
+        console.log(error);
+        reject(error);
+      },
+      () => {
+        getDownloadURL(uploadImage.snapshot.ref)
+          .then((downloadUrl) => resolve(downloadUrl))
+          .catch((error) => reject(error));
+      }
+    );
+  });
+}
 
 export default function AdminAddNewProduct() {
-  // TODO: Complete handle image function.
-  function handleImage() {}
+  async function handleImage(event) {
+    console.log(event.target.files);
+    const extractImageUrl = await helperForUploadingImageToFirebase(
+      event.target.files[0]
+    );
+    console.log(extractImageUrl);
+  }
 
   return (
     <div className="w-full mt-5 mr-0 mb-0 ml-0 relative">
