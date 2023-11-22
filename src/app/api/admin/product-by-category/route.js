baeea43165bf6850696a12fc772c19e33d1b10f3
nainoktv <nainoaktvillegas@gmail.com>
2023-11-21 16:07:09 -0800

    feat: add route to fiter product by category

---
+++ src/app/api/admin/product-by-category/route.js
@@ -0,0 +1,32 @@
+import dbConnection from "@/database";
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function GET(request) {
+  try {
+    await dbConnection();
+    const { searchParams } = new URL(request.url);
+    const id = searchParams.get("id");
+    const getData = await Product.find({ category: id });
+
+    if (getData) {
+      return NextResponse.json({
+        success: true,
+        data: getData,
+      });
+    } else {
+      return NextResponse.json({
+        success: false,
+        status: 204,
+        message: "No Products found...",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong! Please try again later.",
+    });
+  }
+}
