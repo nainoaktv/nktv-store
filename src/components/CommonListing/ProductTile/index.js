51c0146c76496a075f50a5500e0d368d720900cb
nainoktv <nainoaktvillegas@gmail.com>
2023-12-03 12:39:58 -0800

    fix: update button routing

--- src/components/CommonListing/ProductTile/index.js
+++ src/components/CommonListing/ProductTile/index.js
@@ -1,8 +1,11 @@
 "use client";
 
+import { useRouter } from "next/navigation";
+
 export default function ProductTile({ item }) {
+  const router = useRouter();
   return (
-    <div>
+    <div onClick={() => router.push(`/product/${item._id}`)}>
       <div className="overflow-hidden aspect-w-1 aspect-h-1 h-52">
         <img
           src={item.imageUrl}
