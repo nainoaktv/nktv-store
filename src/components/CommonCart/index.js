998ff97a620e3df870a0231213db32f6e2087fb7
nainoktv <nainoaktvillegas@gmail.com>
2023-12-05 15:52:46 -0800

    feat: create cart component for client view

---
+++ src/components/CommonCart/index.js
@@ -0,0 +1,102 @@
+"use client";
+
+// TODO: Add shipping info functionality and remove hard coded price
+export default function CommonCart(cartItems = []) {
+  return (
+    <section className="h-screen bg-gray-100">
+      <div className="mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="mx-auto mt-8 max-w-screen-xl px-4 sm:px-6 lg:px-0">
+          <div className="bg-white shadow">
+            <div className="px-4 py-6 sm:px-8 sm:py-10">
+              <div className="flow-root">
+                {cartItems && cartItems.length ? (
+                  <ul className="-my-8">
+                    {cartItems.map((cartItem) => (
+                      <li
+                        key={cartItem.id}
+                        className="flex-col flex space-y-3 py-6 text-left sm:flex-row sm:space-x-5 sm:space-y-0"
+                      >
+                        <div className="shrink-0">
+                          <img
+                            src={
+                              cartItem &&
+                              cartItem.productID &&
+                              cartItem.productID.imageUrl
+                            }
+                            alt="Product Image"
+                            className="h-24 w-25 max-w-full rounded-lg object-cover"
+                          />
+                        </div>
+                        <div className="flex flex-1 flex-col justify-between">
+                          <div className="sm:col-gap-5 sm:grid sm:grid-cols-2">
+                            <div className="pr-8 sm:pr-4">
+                              <p className="text-base font-semibold text-gray-900">
+                                {cartItem &&
+                                  cartItem.productID &&
+                                  cartItem.productID.productName}
+                              </p>
+                            </div>
+                            <div className="mt-4 flex gap-3 items-end justify-between sm:mt-0 sm:items-start sm:justify-end">
+                              <p className="shrink-0 w-20 text-base font-semibold text-gray-950 sm:order-1 sm:ml-8 sm:text-right">
+                                $
+                                {cartItem &&
+                                  cartItem.productID &&
+                                  cartItem.productID.price}
+                              </p>
+                              <button
+                                type="button"
+                                className="font-medium text-red-400 sm:order-2"
+                              >
+                                Remove
+                              </button>
+                            </div>
+                          </div>
+                        </div>
+                      </li>
+                    ))}
+                  </ul>
+                ) : null}
+              </div>
+              <div className="mt-6 border-t border-b py-2">
+                <div className="flex items-center justify-between">
+                  <p className="text-sm text-gray-400">Subtotal</p>
+                  <p className="text-lg text-gray-400 font-semibold">
+                    $
+                    {cartItems && cartItems.length
+                      ? cartItems.reduce(
+                          (total, item) => item.productID.price + total,
+                          0
+                        )
+                      : "0"}
+                  </p>
+                </div>
+                <div className="flex items-center justify-between">
+                  {/* Shipping Functionality */}
+                  <p className="text-sm text-gray-400">Shipping</p>
+                  <p className="text-lg text-gray-400 font-semibold">$0</p>
+                </div>
+                <div className="flex items-center justify-between">
+                  <p className="text-sm text-gray-400">Total</p>
+                  <p className="text-lg text-gray-400 font-semibold">
+                    $
+                    {cartItems && cartItems.length
+                      ? cartItems.reduce(
+                          (total, item) => item.productID.price + total,
+                          0
+                        )
+                      : "0"}
+                  </p>
+                </div>
+                <div className="mt-5 text-center">
+                  <button className="group inline-flex w-full items-center justify-center bg-black px-6 py-4 text-lg text-white font-medium uppercase tracking-wide">
+                    Checkout
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+}
