d32f654e4977d6c68cd28deef0158e7777936a74
nainoktv <nainoaktvillegas@gmail.com>
2023-12-07 22:44:12 -0800

    chore: order details api

---
+++ src/app/api/order/order-details/route.js
@@ -0,0 +1,49 @@
+import AuthUser from "@/middleware/AuthUser";
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function GET(request) {
+  try {
+    await dbConnection();
+    const isAuthUser = await AuthUser(request);
+
+    if (isAuthUser) {
+      const { searchParams } = new URL(request.url);
+      const id = searchParams.get("id");
+
+      if (!id) {
+        return NextResponse.json({
+          success: false,
+          message: "Product ID is required",
+        });
+      }
+      const extractOrderDetails = await Order.findById(id).populate(
+        "orderItems.product"
+      );
+
+      if (extractOrderDetails) {
+        return NextResponse.json({
+          success: true,
+          data: extractOrderDetails,
+        });
+      } else {
+        return NextResponse.json({
+          success: false,
+          message: "Failed to get order details, please try again.",
+        });
+      }
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "You are not authenticated.",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong! Please try again later.",
+    });
+  }
+}
