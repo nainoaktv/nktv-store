007964b4e4274cf09b0de60a51c243f15164dacc
nainoktv <nainoaktvillegas@gmail.com>
2023-12-07 22:39:16 -0800

    chore: create order and get all orders api route

---
+++ src/app/api/order/create-order/route.js
@@ -0,0 +1,41 @@
+import dbConnection from "@/database";
+import AuthUser from "@/middleware/AuthUser";
+import Cart from "@/models/cart";
+import Order from "@/models/order";
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function POST(request) {
+  try {
+    await dbConnection();
+    const isAuthUser = await AuthUser(request);
+
+    if (isAuthUser) {
+      const data = request.json();
+      const { user } = data;
+
+      const saveNewOrder = await Order.create(data);
+
+      if (saveNewOrder) {
+        await Cart.deleteMany({ userID: user });
+
+        return NextResponse.json({
+          success: true,
+          message: "Products are on the way!",
+        });
+      }
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "Failed to create order, please try again.",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong! Please try again later.",
+    });
+  }
+}
---
+++ src/app/api/order/get-all-orders/route.js
@@ -0,0 +1,44 @@
+import AuthUser from "@/middleware/AuthUser";
+import Order from "@/models/order";
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function GET(request) {
+  try {
+    await dbConnection();
+    const isAuthUser = await AuthUser(request);
+
+    if (isAuthUser) {
+      const { searchParams } = new URL(request.url);
+      const id = searchParams.get("id");
+
+      const extractAllOrders = await Order.find({ user: id }).populate(
+        "orderItems.product"
+      );
+
+      if (extractAllOrders) {
+        return NextResponse.json({
+          success: true,
+          data: extractAllOrders,
+        });
+      } else {
+        return NextResponse.json({
+          success: false,
+          message: "Failed to get all orders, please try again.",
+        });
+      }
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "You are not authenticated.",
+      });
+    }
+  } catch (err) {
+    console.log(err);
+    return NextResponse.json({
+      success: false,
+      message: "Something went wrong! Please try again later.",
+    });
+  }
+}
