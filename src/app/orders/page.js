ca0e680425d3b4a574067e4b6dbfc3b95cf8bf3f
nainoktv <nainoaktvillegas@gmail.com>
2023-12-10 11:12:54 -0800

    chore: create orders route

--- src/app/orders/page.js
+++ src/app/orders/page.js
@@ -1,5 +1,34 @@
 "use client";
 
+import { GlobalContext } from "@/context";
+import { getAllOrdersForUser } from "@/services/order";
+import { useContext, useEffect } from "react";
+import { toast } from "react-toastify";
+
 export default function Orders() {
+  const { user, pageLevelLoader, setPageLevelLoader } =
+    useContext(GlobalContext);
+
+  async function extractAllOrders() {
+    setPageLevelLoader(true);
+    const response = await getAllOrdersForUser(user?._id);
+
+    if (response.success) {
+      setPageLevelLoader(false);
+      toast.success(response.message, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    } else {
+      setPageLevelLoader(false);
+      toast.error(response.message, {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+    }
+  }
+
+  useEffect(() => {
+    if (user !== null) extractAllOrders();
+  }, [user]);
+
   return <section>All your orders</section>;
 }
