e05fdcdd4f8e482b3c32ed70254eca153d1069a3
nainoktv <nainoaktvillegas@gmail.com>
2023-12-15 18:29:00 -0800

    feat: view orders route from account

--- src/app/account/page.js
+++ src/app/account/page.js
@@ -11,6 +11,7 @@ import {
   updateAddress,
 } from "@/services/address";
 import { addNewAddressFormControls } from "@/utils";
+import { useRouter } from "next/navigation";
 import { useContext, useEffect, useState } from "react";
 import { PulseLoader } from "react-spinners";
 import { toast } from "react-toastify";
@@ -32,6 +33,8 @@ export default function Account() {
 
   const [showAddressForm, setShowAddressForm] = useState(false);
   const [currentEditedAddressId, setCurrentEditedAddressId] = useState(null);
+
+  const router = useRouter();
 
   async function extractAllAddresses() {
     setPageLevelLoader(true);
@@ -136,7 +139,10 @@ export default function Account() {
               <p>{user?.email}</p>
               <p>{user?.role}</p>
             </div>
-            <button className="text-white mt-1.5 hover:bg-gray-800 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide">
+            <button
+              onClick={() => router.push("/orders")}
+              className="text-white mt-1.5 hover:bg-gray-800 inline-block bg-black px-5 py-3 text-xs font-medium uppercase tracking-wide"
+            >
               View Your Orders
             </button>
             <div className="mt-6">
