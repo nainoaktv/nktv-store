8d6d8322bb305846eabae08a11d44083ac46d8c1
nainoktv <nainoaktvillegas@gmail.com>
2023-12-15 22:27:46 -0800

    chore: create services for api

--- src/services/order/index.js
+++ src/services/order/index.js
@@ -49,3 +49,37 @@ export const getOrderDetails = async (id) => {
     console.log(err);
   }
 };
+
+export const getAllOrdersForAdmin = async () => {
+  try {
+    const response = await fetch(`/api/admin/orders/get-all-orders`, {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${Cookies.get("token")}`,
+      },
+    });
+
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export const updateOrderStatus = async (formData) => {
+  try {
+    const response = await fetch(`/api/admin/orders/update-order`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${Cookies.get("token")}`,
+      },
+      body: JSON.stringify(formData),
+    });
+
+    const data = await response.json();
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+};
