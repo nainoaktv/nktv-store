6447d969ef6cce368e8e6fa872a92c23d9fce75c
nainoktv <nainoaktvillegas@gmail.com>
2023-12-15 23:24:27 -0800

    fix: import next response

--- src/app/api/admin/orders/update-order/route.js
+++ src/app/api/admin/orders/update-order/route.js
@@ -1,6 +1,7 @@
 import dbConnection from "@/database";
 import AuthUser from "@/middleware/AuthUser";
 import Order from "@/models/order";
+import { NextResponse } from "next/server";
 
 export const dynamic = "force-dynamic";
 
