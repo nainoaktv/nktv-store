e11c1629644af4fb0d0feb97b4d6cdc7e41cafa8
nainoktv <nainoaktvillegas@gmail.com>
2023-12-16 23:11:37 -0800

    style: homepage improvements

--- src/app/page.js
+++ src/app/page.js
@@ -5,8 +5,6 @@ import { getAllAdminProducts } from "@/services/product";
 import { useRouter } from "next/navigation";
 import { useContext, useEffect, useState } from "react";
 
-// TODO: Add source image URL
-
 export default function Home() {
   const { isAuthUser } = useContext(GlobalContext);
 
@@ -36,7 +34,7 @@ export default function Home() {
             <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl">
               Anime & Fitness
             </h1>
-            <p className="max-w-2xl mb-6 font-light lg:mb-8 md:text-lg lg:text-xl">
+            <p className="max-w-2xl mb-6 font-thin lg:mb-8 md:text-lg lg:text-xl">
               Step into the Anime Realm of Style: Where Fashion Meets Fantasy,
               and Every Fit Gives You That Power-Up.
             </p>
@@ -49,7 +47,6 @@ export default function Home() {
             </button>
           </div>
           <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
-            {/* ADD IMAGE SOURCE */}
             <img
               src="https://ae01.alicdn.com/kf/S0701469284a843599513547b698ce5acT.jpg_640x640Q90.jpg_.webp"
               alt="View Collection"
@@ -77,7 +74,13 @@ export default function Home() {
                       .filter((item) => item.onSale === "yes")
                       .splice(0, 2)
                       .map((productItem) => (
-                        <li key={productItem._id}>
+                        <li
+                          onClick={() =>
+                            router.push(`/product/${productItem._id}`)
+                          }
+                          className="cursor-pointer"
+                          key={productItem._id}
+                        >
                           <div>
                             <img
                               src={productItem.imageUrl}
@@ -91,7 +94,7 @@ export default function Home() {
                             </h3>
                             <p className="mt-1 text-sm">
                               ${productItem.price}{" "}
-                              <span>{`(-${productItem.priceDrop})`}</span>
+                              <span>{`(-${productItem.priceDrop}%) Off`}</span>
                             </p>
                           </div>
                         </li>
@@ -100,6 +103,66 @@ export default function Home() {
               </ul>
             </div>
           </div>
+        </div>
+        <div className="max-w-screen-xl px-4 py-8 mx-auto sm:px-6 sm:py-12 lg:px-8">
+          <div className="text-center">
+            <h2 className="text-xl text-white sm:text-3xl uppercase font-thin mb-4 pb-2 border-b border-white">
+              Shop By Category
+            </h2>
+          </div>
+          <ul className="grid grid-cols-1 gap-4 lg:grid-cols-3">
+            <li>
+              <div className="relative block group">
+                <img
+                  src="https://lzd-img-global.slatic.net/g/p/67de87f87faa3750f390e2bd579784d5.jpg_720x720q80.jpg"
+                  className="object-cover w-full aspect-square"
+                />
+                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                  <h3 className="text-xl font-bold">KIDS</h3>
+                  <button
+                    onClick={() => router.push("/product/listing/kids")}
+                    className="mt-1.5 inline-block bg-black hover:bg-gray-800 text-white px-5 py-3 text-xs font-medium uppercase tracking-wide"
+                  >
+                    Shop Now
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li>
+              <div className="relative block group">
+                <img
+                  src="https://i.etsystatic.com/17820519/r/il/b82106/5291409104/il_570xN.5291409104_8egs.jpg"
+                  className="object-cover w-full aspect-square"
+                />
+                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                  <h3 className="text-xl font-bold">WOMEN</h3>
+                  <button
+                    onClick={() => router.push("/product/listing/women")}
+                    className="mt-1.5 inline-block bg-white hover:bg-gray-400 text-black px-5 py-3 text-xs font-medium uppercase tracking-wide"
+                  >
+                    Shop Now
+                  </button>
+                </div>
+              </div>
+            </li>
+            <li className="lg:col-span-2 lg:col-start-2 lg:row-span-2 lg:row-start-1">
+              <div className="relative block group">
+                <img
+                  src="https://i.etsystatic.com/38844348/r/il/514eb8/4376443559/il_fullxfull.4376443559_fspy.jpg"
+                  className="object-cover w-full aspect-square"
+                />
+                <div className="absolute inset-0 flex flex-col items-start justify-end p-6">
+                  <h3 className="text-black text-xl font-bold">MEN</h3>
+                  <button
+                    onClick={() => router.push("/product/listing/men")}
+                    className="mt-1.5 inline-block bg-black hover:bg-gray-800 text-white px-5 py-3 text-xs font-medium uppercase tracking-wide"
+                  >
+                    Shop Now
+                  </button>
+                </div>
+              </div>
+            </li>
+          </ul>
         </div>
       </section>
     </main>
