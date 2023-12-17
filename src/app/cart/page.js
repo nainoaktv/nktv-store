0f29177839e716f3b20cb20c960611c9b30fa66e
nainoktv <nainoaktvillegas@gmail.com>
2023-12-16 23:44:04 -0800

    chore: price drop logic

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -23,7 +23,26 @@ export default function Cart() {
     const response = await getAllCartItems(user?._id);
 
     if (response.success) {
-      setCartItems(response.data);
+      const updatedData =
+        response.data && response.data.length
+          ? response.data.map((item) => ({
+              ...item,
+              productID: {
+                ...item.productID,
+                price:
+                  item.productID.onSale === "yes"
+                    ? parseInt(
+                        (
+                          item.productID.price -
+                          item.productID.price *
+                            (item.productID.priceDrop / 100)
+                        ).toFixed(2)
+                      )
+                    : item.productID.price,
+              },
+            }))
+          : [];
+      setCartItems(updatedData);
       setPageLevelLoader(false);
       localStorage.setItem("cartItems", JSON.stringify(response.data));
     }
